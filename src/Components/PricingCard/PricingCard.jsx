f828dbca1db84d6b55feb025fbcfdcb93cc8f7a2
piasmajumdar <piasmajumdar@gmail.com>
2026-03-24 18:57:51 +0530

    card design done by daisyUI

--- src/Components/PricingCard/PricingCard.jsx
+++ src/Components/PricingCard/PricingCard.jsx
@@ -3,17 +3,17 @@ import PricingFeature from './PricingFeature';
 
 const PricingCard = ({pricing}) => {
 
-    const {name, price, description, features} = pricing;
+    const {name, price, description, features, duration} = pricing;
 
     return (
-        <div  className='border p-4 bg-amber-600 rounded-2xl'>
+        <div  className='border flex flex-col p-4 bg-amber-600 rounded-2xl'>
             {/* Card header */}
             <div>
                 <h1 className='text-7xl'>{name}</h1>
-                <h4 className='text-3xl'>{price} $</h4>
+                <h4 className='text-3xl'>${price}/{duration}</h4>
             </div>
             {/* Card body */}
-            <div className='bg-amber-400 p-4 rounded-2xl mt-10'>
+            <div className='bg-amber-400 p-4 rounded-2xl mt-10 flex-1'>
                 <p>{description}</p>
                 {
                     features.map((feature, index)=> <PricingFeature
@@ -21,6 +21,7 @@ const PricingCard = ({pricing}) => {
                         feature={feature}></PricingFeature>)
                 }
             </div>
+            <button className="btn max-w-full mt-4">Subscribe</button>
         </div>
     );
 };
--- src/Components/PricingOptions/PricingOptions.jsx
+++ src/Components/PricingOptions/PricingOptions.jsx
@@ -1,5 +1,6 @@
 import { use } from "react";
 import PricingCard from "../PricingCard/PricingCard";
+import DaisyPricing from "../DaisyPricing/DaisyPricing";
 
 
 const PricingOptions = ({ pricingPromise }) => {
@@ -11,8 +12,14 @@ const PricingOptions = ({ pricingPromise }) => {
         <div>
             <h2 className='text-5xl'>Get our Membership</h2>
 
-            <div className="grid grid-cols-3 gap-8">
-                {pricingData.map(pricing => <PricingCard key={pricing.id} pricing={pricing}></PricingCard>)}
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {/* {pricingData.map(pricing => <PricingCard
+                    key={pricing.id}
+                    pricing={pricing}></PricingCard>)} */}
+
+                {pricingData.map(pricing => <DaisyPricing
+                    key={pricing.id}
+                    pricing={pricing}></DaisyPricing>)}
             </div>
         </div>
     );
