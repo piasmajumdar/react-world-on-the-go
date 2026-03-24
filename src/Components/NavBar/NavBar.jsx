d72fa88cef342f8a48e505f4813a61889c6bff07
piasmajumdar <piasmajumdar@gmail.com>
2026-03-24 13:56:48 +0530

    menu icon toggle and menu list responsive

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,40 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './Link';
+import { Menu, X } from 'lucide-react';
 
 const navigationData = [
-  {
-    id: 1,
-    name: "Home",
-    url: "/"
-  },
-  {
-    id: 2,
-    name: "About",
-    url: "/about"
-  },
-  {
-    id: 3,
-    name: "Services",
-    url: "/services"
-  },
-  {
-    id: 4,
-    name: "Blog",
-    url: "/blog"
-  },
-  {
-    id: 5,
-    name: "Contact",
-    url: "/contact"
-  }
+    {
+        id: 1,
+        name: "Home",
+        url: "/"
+    },
+    {
+        id: 2,
+        name: "About",
+        url: "/about"
+    },
+    {
+        id: 3,
+        name: "Services",
+        url: "/services"
+    },
+    {
+        id: 4,
+        name: "Blog",
+        url: "/blog"
+    },
+    {
+        id: 5,
+        name: "Contact",
+        url: "/contact"
+    }
 ];
 
 const NavBar = () => {
-    return (
-        <nav>
+    const [open, setOpen] = useState(false)
 
-            <ul className='flex'>
-                {navigationData.map(route => <Link key={route.id} route={route}></Link>)}
+    const links = navigationData.map(route => <Link key={route.id} route={route}></Link>)
+
+    return (
+        <nav className='flex justify-between mx-8 my-4'>
+
+            <span className='flex' onClick={() => setOpen(!open)}>
+                {
+                    open ?
+                        <X className='md:hidden'></X> :
+                        <Menu className='md:hidden'></Menu>
+                }
+                <ul className='md:hidden'>
+                    {links}
+                </ul>
+                <h3 className='ml-4'>My Navbar</h3>
+            </span>
+            <ul className='hidden md:flex'>
+                {links}
             </ul>
 
             {/* <ul className='flex'>
@@ -46,7 +62,9 @@ const NavBar = () => {
                 <li className='mr-10'><a href="./About">About</a></li>
                 <li className='mr-10'><a href="./Blog">Blog</a></li>
             </ul> */}
-            
+
+            <button>Button</button>
+
         </nav>
     );
 };
